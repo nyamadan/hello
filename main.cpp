845482bd2826b5b690da39297f13b83933ea3e7a
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-05-05 23:53:56 +0900

    AlignedDelegeter

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
@@ -8,9 +9,6 @@
 #include <embree3/rtcore_geometry.h>
 #include <embree3/rtcore_ray.h>
 
-glm::vec3 *face_colors = nullptr;
-glm::vec3 *vertex_colors = nullptr;
-
 #ifdef _MSC_VER
 #define aligned_alloc _aligned_malloc
 #define aligned_free _aligned_free
@@ -18,57 +16,61 @@ glm::vec3 *vertex_colors = nullptr;
 #define aligned_free free
 #endif
 
-template <typename T>
+template <typename T = void>
 struct AlignedDeleter {
-  void operator()(T *ptr) const {
-	  aligned_free(ptr);
-  }
+  constexpr AlignedDeleter() noexcept = default;
+
+  template <typename U,
+            typename std::enable_if<std::is_convertible<U *, T *>::value,
+                                    std::nullptr_t>::type = nullptr>
+  AlignedDeleter(const AlignedDeleter<U> &) noexcept {}
+
+  void operator()(T *ptr) const { aligned_free(ptr); }
+};
+
+template <typename T>
+struct AlignedDeleter<T[]> {
+  constexpr AlignedDeleter() noexcept = default;
+
+  template <typename U, typename std::enable_if<
+                            std::is_convertible<U (*)[], T (*)[]>::value,
+                            std::nullptr_t>::type = nullptr>
+  AlignedDeleter(const AlignedDeleter<U[]> &) noexcept {}
+
+  void operator()(T *ptr) const { aligned_free(ptr); }
 };
 
 /* adds a cube to the scene */
-unsigned int addCube(RTCDevice device, RTCScene scene) {
+unsigned int addCube(
+    RTCDevice device, RTCScene scene,
+    std::unique_ptr<glm::vec3[], AlignedDeleter<glm::vec3[]>> &vertex_colors,
+    std::unique_ptr<glm::vec3[], AlignedDeleter<glm::vec3[]>> &face_colors) {
   /* create a triangulated cube with 12 triangles and 8 vertices */
   auto mesh = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
 
   /* create face and vertex color arrays */
-  face_colors = (glm::vec3 *)aligned_alloc(12 * sizeof(glm::vec3), 16);
-  vertex_colors = (glm::vec3 *)aligned_alloc(8 * sizeof(glm::vec3), 16);
+  face_colors.reset((glm::vec3 *)aligned_alloc(12 * sizeof(glm::vec3), 16));
+  vertex_colors.reset((glm::vec3 *)aligned_alloc(8 * sizeof(glm::vec3), 16));
 
   /* set vertices and vertex colors */
   auto *vertices = (glm::vec3 *)rtcSetNewGeometryBuffer(
       mesh, RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, sizeof(glm::vec3), 8);
   vertex_colors[0] = glm::vec3(0, 0, 0);
-  vertices[0].x = -1;
-  vertices[0].y = -1;
-  vertices[0].z = -1;
+  vertices[0] = glm::ivec3(-1, -1, -1);
   vertex_colors[1] = glm::vec3(0, 0, 1);
-  vertices[1].x = -1;
-  vertices[1].y = -1;
-  vertices[1].z = +1;
+  vertices[1] = glm::ivec3(-1, -1, +1);
   vertex_colors[2] = glm::vec3(0, 1, 0);
-  vertices[2].x = -1;
-  vertices[2].y = +1;
-  vertices[2].z = -1;
+  vertices[2] = glm::ivec3(-1, +1, -1);
   vertex_colors[3] = glm::vec3(0, 1, 1);
-  vertices[3].x = -1;
-  vertices[3].y = +1;
-  vertices[3].z = +1;
+  vertices[3] = glm::ivec3(-1, +1, +1);
   vertex_colors[4] = glm::vec3(1, 0, 0);
-  vertices[4].x = +1;
-  vertices[4].y = -1;
-  vertices[4].z = -1;
+  vertices[4] = glm::ivec3(+1, -1, -1);
   vertex_colors[5] = glm::vec3(1, 0, 1);
-  vertices[5].x = +1;
-  vertices[5].y = -1;
-  vertices[5].z = +1;
+  vertices[5] = glm::ivec3(+1, -1, +1);
   vertex_colors[6] = glm::vec3(1, 1, 0);
-  vertices[6].x = +1;
-  vertices[6].y = +1;
-  vertices[6].z = -1;
+  vertices[6] = glm::ivec3(+1, +1, -1);
   vertex_colors[7] = glm::vec3(1, 1, 1);
-  vertices[7].x = +1;
-  vertices[7].y = +1;
-  vertices[7].z = +1;
+  vertices[7] = glm::ivec3(+1, +1, +1);
 
   /* set triangles and face colors */
   auto tri = 0;
@@ -125,7 +127,7 @@ unsigned int addCube(RTCDevice device, RTCScene scene) {
 
   rtcSetGeometryVertexAttributeCount(mesh, 1);
   rtcSetSharedGeometryBuffer(mesh, RTC_BUFFER_TYPE_VERTEX_ATTRIBUTE, 0,
-                             RTC_FORMAT_FLOAT3, vertex_colors, 0,
+                             RTC_FORMAT_FLOAT3, vertex_colors.get(), 0,
                              sizeof(glm::vec3), 8);
 
   rtcCommitGeometry(mesh);
@@ -168,17 +170,16 @@ unsigned int addGroundPlane(RTCDevice device, RTCScene scene) {
 }
 
 int main(void) {
+  auto face_colors =
+      std::unique_ptr<glm::vec3[], AlignedDeleter<glm::vec3[]>>();
+  auto vertex_colors =
+      std::unique_ptr<glm::vec3[], AlignedDeleter<glm::vec3[]>>();
+
   auto device = rtcNewDevice(NULL);
   auto scene = rtcNewScene(device);
 
-  auto cube = addCube(device, scene);
+  auto cube = addCube(device, scene, vertex_colors, face_colors);
   auto plane = addGroundPlane(device, scene);
-
-   std::unique_ptr<glm::vec3, AlignedDeleter<glm::vec3>> x(
-      (glm::vec3 *)aligned_alloc(12 * sizeof(glm::vec3), 16));
-
-  aligned_free(face_colors);
-  aligned_free(vertex_colors);
 
   rtcReleaseScene(scene);
   rtcReleaseDevice(device);
