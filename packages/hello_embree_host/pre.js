f0b83bf5ffb6b82f47d2415ce6707bdf94a9ab7b
Yoshiki YAMADA <nyamadandan@gmail.com>
2022-09-24 14:28:08 +0900

    dev container

--- packages/hello_embree_host/pre.js
+++
@@ -1,6 +0,0 @@
-Module["postRun"] = async function() {
-  const response = await fetch("/main.lua");
-  const text = await response.text();
-  FS.createDataFile("/", "main.lua", text, true, true, false);
-  callMain(["-f", "./main.lua"]);
-}
