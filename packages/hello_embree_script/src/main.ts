507e84c7ad4d8a305ced3964ebf5db84ed9e4778
Yoshiki YAMADA <nyamadandan@gmail.com>
2022-09-24 17:32:21 +0900

    WIP web interface

---
+++ packages/hello_embree_script/src/main.ts
@@ -0,0 +1,23 @@
+// @ts-ignore
+import createModule from "hello_embree_host";
+const Module: any = await createModule();
+const { FS, callMain } = Module;
+
+const canvas = document.querySelector("#canvas");
+if (canvas == null) {
+  throw new Error("Could not find #canvas");
+}
+canvas.addEventListener(
+  "webglcontextlost",
+  function (e) {
+    alert("WebGL context lost. You will need to reload the page.");
+    e.preventDefault();
+  },
+  false
+);
+Module["canvas"] = canvas;
+
+const response = await fetch("/main.lua");
+const text = await response.text();
+FS.createDataFile("/", "main.lua", text, true, true, false);
+callMain(["-f", "/main.lua"]);
