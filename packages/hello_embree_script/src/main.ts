65cfe956dcdc59611346b9ce5478ac52e4e51c4a
Yoshiki YAMADA <nyamadandan@gmail.com>
2022-09-25 19:54:59 +0900

    async main

--- packages/hello_embree_script/src/main.ts
+++ packages/hello_embree_script/src/main.ts
@@ -1,23 +1,28 @@
 // @ts-ignore
 import createModule from "hello_embree_host";
-const Module: any = await createModule();
-const { FS, callMain } = Module;
 
-const canvas = document.querySelector("#canvas");
-if (canvas == null) {
-  throw new Error("Could not find #canvas");
+async function main() {
+  const Module: any = await createModule();
+  const { FS, callMain } = Module;
+
+  const canvas = document.querySelector("#canvas");
+  if (canvas == null) {
+    throw new Error("Could not find #canvas");
+  }
+  canvas.addEventListener(
+    "webglcontextlost",
+    function (e) {
+      alert("WebGL context lost. You will need to reload the page.");
+      e.preventDefault();
+    },
+    false
+  );
+  Module["canvas"] = canvas;
+
+  const response = await fetch("/main.lua");
+  const text = await response.text();
+  FS.createDataFile("/", "main.lua", text, true, true, false);
+  callMain(["-f", "/main.lua"]);
 }
-canvas.addEventListener(
-  "webglcontextlost",
-  function (e) {
-    alert("WebGL context lost. You will need to reload the page.");
-    e.preventDefault();
-  },
-  false
-);
-Module["canvas"] = canvas;
 
-const response = await fetch("/main.lua");
-const text = await response.text();
-FS.createDataFile("/", "main.lua", text, true, true, false);
-callMain(["-f", "/main.lua"]);
+main();
