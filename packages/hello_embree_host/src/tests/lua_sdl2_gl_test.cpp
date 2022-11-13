0beae95e8bf822d1aeb2140e20afd30fc6f8eadc
Yoshiki YAMADA <nyamadandan@gmail.com>
2022-11-13 16:14:17 +0900

    Skip tests for GitHub Actions

--- packages/hello_embree_host/src/tests/lua_sdl2_gl_test.cpp
+++ packages/hello_embree_host/src/tests/lua_sdl2_gl_test.cpp
@@ -143,6 +143,10 @@ TEST_F(LuaSDL2_Test, TestglClear) {
 }
 
 TEST_F(LuaSDL2_Test, TestVAO) {
+#if defined(RUN_ON_GITHUB_ACTIONS)
+  GTEST_SKIP() << "Not work for GitHub Actions";
+#endif
+
 #if defined(__EMSCRIPTEN__)
   GTEST_SKIP() << "Not work for Emscripten";
 #endif
