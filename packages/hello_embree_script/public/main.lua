5612a1255ff8a0326da79e06f04a27a708c11eb4
Yoshiki YAMADA <nyamadandan@gmail.com>
2022-11-13 18:02:33 +0900

    format lua script

--- packages/hello_embree_script/public/main.lua
+++ packages/hello_embree_script/public/main.lua
@@ -31,48 +31,48 @@ void main(void) {
     fragColor = vec4(color, 1.0);
 }
 ]]
-        local program = glslang.newProgram();
-        local vsShader = glslang.newShader(glslang.EShLangVertex);
-        vsShader:setString(DefaultVertexShader);
-        vsShader:setEnvInput(glslang.EShSourceGlsl, glslang.EShLangVertex, glslang.EShClientOpenGL, 100);
-        vsShader:setEnvClient(glslang.EShClientOpenGL, glslang.EShTargetOpenGL_450);
-        vsShader:setEnvTarget(glslang.EShTargetSpv, glslang.EShTargetSpv_1_0);
+        local program = glslang.newProgram()
+        local vsShader = glslang.newShader(glslang.EShLangVertex)
+        vsShader:setString(DefaultVertexShader)
+        vsShader:setEnvInput(glslang.EShSourceGlsl, glslang.EShLangVertex, glslang.EShClientOpenGL, 100)
+        vsShader:setEnvClient(glslang.EShClientOpenGL, glslang.EShTargetOpenGL_450)
+        vsShader:setEnvTarget(glslang.EShTargetSpv, glslang.EShTargetSpv_1_0)
         if not vsShader:parse(100, true, 0) then
             error(vsShader:getInfoLog())
         end
-        program:addShader(vsShader);
+        program:addShader(vsShader)
 
-        local fsShader = glslang.newShader(glslang.EShLangFragment);
-        fsShader:setString(DefaultFragmentShader);
-        fsShader:setEnvInput(glslang.EShSourceGlsl, glslang.EShLangFragment, glslang.EShClientOpenGL, 100);
-        fsShader:setEnvClient(glslang.EShClientOpenGL, glslang.EShTargetOpenGL_450);
-        fsShader:setEnvTarget(glslang.EShTargetSpv, glslang.EShTargetSpv_1_0);
+        local fsShader = glslang.newShader(glslang.EShLangFragment)
+        fsShader:setString(DefaultFragmentShader)
+        fsShader:setEnvInput(glslang.EShSourceGlsl, glslang.EShLangFragment, glslang.EShClientOpenGL, 100)
+        fsShader:setEnvClient(glslang.EShClientOpenGL, glslang.EShTargetOpenGL_450)
+        fsShader:setEnvTarget(glslang.EShTargetSpv, glslang.EShTargetSpv_1_0)
         if not fsShader:parse(100, true, 0) then
             error(fsShader:getInfoLog())
         end
-        program:addShader(fsShader);
+        program:addShader(fsShader)
 
         if not program:link(0) then
             error(program:getInfoLog())
         end
 
-        local vsIntermediate = program:getIntermediate(glslang.EShLangVertex);
-        local vsSpv = glslang.glslangToSpv(program, vsIntermediate);
+        local vsIntermediate = program:getIntermediate(glslang.EShLangVertex)
+        local vsSpv = glslang.glslangToSpv(program, vsIntermediate)
 
-        local fsIntermediate = program:getIntermediate(glslang.EShLangFragment);
-        local fsSpv = glslang.glslangToSpv(program, fsIntermediate);
+        local fsIntermediate = program:getIntermediate(glslang.EShLangFragment)
+        local fsSpv = glslang.glslangToSpv(program, fsIntermediate)
 
         if utils.isEmscripten() then
-            print(spv_cross.compile(vsSpv, {es = true, version = 300}));
-            print(spv_cross.compile(fsSpv, {es = true, version = 300}));
+            print(spv_cross.compile(vsSpv, { es = true, version = 300 }))
+            print(spv_cross.compile(fsSpv, { es = true, version = 300 }))
         else
-            print(spv_cross.compile(vsSpv, {es = false, version = 420}));
-            print(spv_cross.compile(fsSpv, {es = false, version = 420}));
+            print(spv_cross.compile(vsSpv, { es = false, version = 420 }))
+            print(spv_cross.compile(fsSpv, { es = false, version = 420 }))
         end
     end
 
     local function collectEvents()
-        local events = {};
+        local events = {}
         while true do
             local event = SDL.PollEvent()
             if event == nil then
@@ -81,25 +81,26 @@ void main(void) {
             table.insert(events, event)
         end
 
-        return events;
+        return events
     end
 
-    glslang.initializeProcess();
+    glslang.initializeProcess()
 
-    transpileShaders();
+    transpileShaders()
 
     if SDL.Init(SDL.INIT_VIDEO | SDL.INIT_TIMER) ~= 0 then
         error(SDL.GetError())
     end
 
-    local window = SDL.CreateWindow("Hello World!!", SDL.WINDOWPOS_UNDEFINED, SDL.WINDOWPOS_UNDEFINED, 1280, 720, SDL.WINDOW_OPENGL)
+    local window = SDL.CreateWindow("Hello World!!", SDL.WINDOWPOS_UNDEFINED, SDL.WINDOWPOS_UNDEFINED, 1280, 720,
+        SDL.WINDOW_OPENGL)
     local renderer = SDL.CreateRenderer(window, -1, SDL.RENDERER_ACCELERATED)
 
     SDL.GL_SetAttribute(SDL.GL_CONTEXT_FLAGS, 0)
     SDL.GL_SetAttribute(SDL.GL_CONTEXT_PROFILE_MASK, SDL.GL_CONTEXT_PROFILE_CORE)
     SDL.GL_SetAttribute(SDL.GL_CONTEXT_MAJOR_VERSION, 3)
     SDL.GL_SetAttribute(SDL.GL_CONTEXT_MINOR_VERSION, 0)
-    local context = SDL.GL_CreateContext(window);
+    local context = SDL.GL_CreateContext(window)
     if context == nil then
         error("Failed: SDL.GL_CreateContext")
     end
@@ -110,22 +111,22 @@ void main(void) {
         gl.loadGLLoader()
     end
 
-    local points = {-1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 0.0,  1.0, 0.0 };
-    local buffer = buffer.alloc(#points * 4);
-    for i,v in ipairs(points) do
-        buffer:setFloat32(4 * (i - 1), v);
+    local points = { -1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 0.0 }
+    local buffer = buffer.alloc(#points * 4)
+    for i, v in ipairs(points) do
+        buffer:setFloat32(4 * (i - 1), v)
     end
 
-    local vbo = gl.genBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, 0);
+    local vbo = gl.genBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
+    gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.STATIC_DRAW)
+    gl.bindBuffer(gl.ARRAY_BUFFER, 0)
 
-    local vao = gl.genVertexArray();
-    gl.bindVertexArray(vao);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.enableVertexAttribArray(0);
-    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 0);
+    local vao = gl.genVertexArray()
+    gl.bindVertexArray(vao)
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
+    gl.enableVertexAttribArray(0)
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 0)
 
     local function update()
         local events = collectEvents()
@@ -142,9 +143,9 @@ void main(void) {
         gl.clearColor(1, 0, 1, 1)
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-        gl.bindVertexArray(gl.ARRAY_BUFFER, vao);
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
-        gl.bindVertexArray(gl.ARRAY_BUFFER, 0);
+        gl.bindVertexArray(gl.ARRAY_BUFFER, vao)
+        gl.drawArrays(gl.TRIANGLES, 0, 3)
+        gl.bindVertexArray(gl.ARRAY_BUFFER, 0)
 
         SDL.GL_SwapWindow(window)
     end
